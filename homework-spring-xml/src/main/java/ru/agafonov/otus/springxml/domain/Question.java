6a804bec924f46499a18e2eb4a4e8c67ae360b04
aga4n <e.aga4nov@yandex.ru>
2019-08-29 18:15:28 +0300

    add question domain,lombok, opencsv, slf4j, spring

---
+++ homework-spring-xml/src/main/java/ru/agafonov/otus/springxml/domain/Question.java
@@ -0,0 +1,22 @@
+package ru.agafonov.otus.springxml.domain;
+
+import com.opencsv.bean.CsvBindByName;
+
+public class Question {
+
+    @CsvBindByName(column = "Text Question")
+    private String text;
+
+
+    @CsvBindByName(column = "Answer")
+    private String answer;
+
+    public String getText() {
+        return this.text;
+    }
+
+    public String getAnswer() {
+        return this.answer;
+    }
+
+}
